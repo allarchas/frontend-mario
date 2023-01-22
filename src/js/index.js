ab4e0c40687758c2748394c5c2c0e90d876a1b26
allarchas <79531331+allarchas@users.noreply.github.com>
2023-01-22 18:07:13 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+const botaoTrailer = document.querySelector('.botao-trailer');
+const modal = document.querySelector('.modal');
+const fecharModal = document.querySelector('.fechar-modal');
+const video = document.getElementById('video');
+const linkVideo = video.src;
+
+function alternarModal(){
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkVideo);
+});
+
+fecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
+
